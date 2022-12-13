be85b107446006e44e1abd9220f3b4e65219eda0
bhagyashrigalpalli1234 <47694999+bhagyashrigalpalli1234@users.noreply.github.com>
2022-12-13 08:43:23 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,45 @@
+//second largest element
+import java.util.Scanner;
+public class array {
+    static int findmax(int arr[])
+    {
+        int max=Integer.MIN_VALUE;
+        int i;
+        for(i=0;i<arr.length;i++)
+        {
+            if(arr[i]>max)
+                max=arr[i];
+        }
+        return max;
+
+    }
+
+    static int findsecondmax(int arr[])
+    {
+        int mx=findmax(arr);
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==mx)
+                arr[i]=Integer.MIN_VALUE;
+        }
+        int secondmx=findmax(arr);
+
+        return secondmx;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println(findsecondmax(arr));
+
+
+    }
+}
+
